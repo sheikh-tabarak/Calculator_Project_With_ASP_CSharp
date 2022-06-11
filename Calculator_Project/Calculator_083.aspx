8fba7522261b0235f14cb6763826f043c723d6ca
Muhammad Tabarak <Muhammad Tabarak@MUHAMMAD-TABARA>
2022-06-11 16:08:45 +0500

    Add project files.

---
+++ Calculator_Project/Calculator_083.aspx
@@ -0,0 +1,275 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Calculator_083.aspx.cs" Inherits="Calculator_Project.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Calculator | 19011598-083 </title>
+
+      <style>
+
+#maindiv{
+background-color: black;
+color: white;
+margin-left: 34%!important;
+margin-right: 34%!important;
+padding: 20px;
+padding-top: 10px;
+border-radius: 20px;
+text-align: center;
+}
+@media only screen and (max-width: 768px) {
+  /* For mobile phones: */
+ #maindiv {
+      margin: 5%!important; 
+          }
+}
+
+
+.input-field{
+    background-color: rgb(48, 48, 48);
+    border-width: 0px;
+    width: 95%;
+    height: 90px;
+    border-radius: 20px;
+    color: white;
+    font: 500;
+    font-size: 26px;
+    text-align: right;
+    padding:10px;
+}
+
+.number-button{
+    background-color: rgb(48, 48, 48);
+    padding: 10px;
+    color:white;
+    height: 50px;
+    width: 60px;
+    border-radius: 30px;
+   font-size: 20px;
+   font-weight: 500;
+   margin: 5px;
+}
+
+.number-button:hover{
+    background-color: rgb(72, 72, 72);
+}
+
+
+.operator-button{
+    background-color: rgb(44, 45, 63);
+    padding: 10px;
+    color:rgb(118, 145, 207);
+    height: 50px;
+    width: 60px;
+    border-radius: 30px;
+   font-size: 20px;
+   font-weight: 500;
+   margin: 5px;
+}
+
+.color-button{
+background-color: red;
+    padding: 5px;
+    color:rgb(255, 255, 255);
+    height: 50px;
+    width: 78px;
+    border-radius: 30px;
+   font-size: 14px;
+   font-weight:500;
+   margin: 5px;
+}
+
+#color-red{
+background-color: red;
+}
+
+#color-green{
+background-color: green;
+}
+
+#color-blue{
+background-color: blue;
+}
+
+.done-button{
+    background-color: rgb(118, 145, 207);
+    padding: 5px;
+    color:black;
+    height: 50px;
+    width: 78px;
+    border-radius: 30px;
+   font-size: 12px;
+   font-weight: 500;
+   margin: 5px;
+}
+
+.clear-button{
+    background-color: rgb(118, 145, 207);
+    padding: 10px;
+    color:rgb(0, 0, 0);
+    height: 50px;
+    width: 60px;
+    border-radius: 30px;
+   font-size: 20px;
+   font-weight: 500;
+   margin: 5px;
+}
+
+.color-radio{
+    color: white;
+}
+
+.heading{
+    background-color: white;
+    color: black;
+    font-size: 30px;
+    padding: 5px;
+    text-align:center;
+    border-radius: 15px;
+    width:100%;
+
+}
+
+.error-message{
+    color: red;
+    text-align: center;
+    font-size: 12px;
+}
+
+          .auto-style1 {
+              width: 100%;
+          }
+
+          .auto-style2 {
+              width: 891px;
+          }
+
+          #blackcolor{
+              border-radius:20px;
+              width:100px!important;
+              border:solid 1px white;
+          }
+
+          .auto-style3 {
+              background-color: rgb(48, 48, 48);
+              border-width: 0px;
+              border-radius: 20px;
+              color: white;
+              font-size: 26px;
+              text-align: right;
+              padding: 10px;
+          }
+
+    </style>
+
+
+</head>
+<body>
+    <form id="form1" runat="server">
+ 
+  
+       
+          <asp:Panel ID="maindiv" runat="server" CssClass="maindiv">
+        <div id="mainiv" class="Main">
+        
+        <table class="auto-style1" >
+            <tr>
+                <td>
+                    <div class="heading">
+
+                    <asp:Label CssClass="heading" ID="Label1" runat="server" Text="Calculator"></asp:Label>
+               </div>
+                    </td>
+            </tr>
+        </table>
+ 
+     
+
+        <table class="auto-style1">
+            <tr>
+                <td class="auto-style2">
+                    <asp:Label CssClass="error-message" ID="errormessage" runat="server" Text="Label"></asp:Label>
+                </td>
+                <td>
+                    
+                    <asp:Button ID="blackcolor" runat="server" BackColor="Black" ForeColor="White" Height="33px" OnClick="black_Click" Text="Black Theme" Width="100%" />
+                    
+                </td>
+            </tr>
+        </table>
+        <table class="auto-style1">
+            <tr>
+                <td>
+                    &nbsp;</td>
+
+            </tr>
+        </table>
+
+            <table class="auto-style1">
+            <tr>
+                <td>
+                    <asp:TextBox  CssClass="input-field auto-style3" ID="TextBox1" runat="server" Height="16px" Enabled="False">0</asp:TextBox>
+                </td>
+            </tr>
+        </table>
+ 
+     
+
+        <table class="auto-style1">
+            <tr>
+                <td>
+                    <asp:Button OnClick="color_red_Click" CssClass="color-button" ID="red" runat="server" Text="Red"  />
+                </td>
+                <td><asp:Button CssClass="color-button" ID="green" runat="server" Text="Green" BackColor="Green" OnClick="green_Click" /></td>
+                <td><asp:Button CssClass="color-button" ID="blue" runat="server" Text="Blue" BackColor="Blue" OnClick="blue_Click" /></td>
+                <td><asp:Button CssClass="done-button" ID="Calculate" runat="server" Text="Calculate" OnClick="Calculate_Click" /></td>
+
+            </tr>
+        </table>
+ 
+            
+        <table class="auto-style1">
+            <tr>
+                <td>
+                    <asp:Button CssClass="number-button" ID="N7" runat="server" Text="7" OnClick="N7_Click" />
+                </td>
+                <td><asp:Button CssClass="number-button" ID="N8" runat="server" Text="8" OnClick="N8_Click" /></td>
+                <td><asp:Button CssClass="number-button" ID="N9" runat="server" Text="9" OnClick="N9_Click" /></td>
+                <td><asp:Button CssClass="operator-button" ID="Oplus" runat="server" Text="+" OnClick="Oplus_Click" /></td>
+                <td><asp:Button CssClass="operator-button" ID="Oback" runat="server" Text="&#8592;" OnClick="Oback_Click" /></td>
+            </tr>
+            <tr>
+                <td><asp:Button CssClass="number-button" ID="N4" runat="server" Text="4" OnClick="N4_Click" /></td>
+                <td><asp:Button CssClass="number-button" ID="N5" runat="server" Text="5" OnClick="N5_Click" /></td>
+               <td><asp:Button CssClass="number-button" ID="N6" runat="server" Text="6" OnClick="N6_Click" /></td>
+                <td><asp:Button CssClass="operator-button" ID="Ominus" runat="server" Text="-" OnClick="Ominus_Click" /></td>
+                 <td><asp:Button CssClass="operator-button" ID="Oup" runat="server" Text="&#8593;" OnClick="Oup_Click" Enabled="False" /></td>
+            </tr>
+            <tr>
+              <td><asp:Button CssClass="number-button" ID="N1" runat="server" Text="1" OnClick="N1_Click" /></td>
+              <td><asp:Button CssClass="number-button" ID="N2" runat="server" Text="2" OnClick="N2_Click" /></td>
+              <td><asp:Button CssClass="number-button" ID="N3" runat="server" Text="3" OnClick="N3_Click" /></td>
+             <td><asp:Button CssClass="operator-button" ID="Omultiply" runat="server" Text="*" OnClick="Omultiply_Click" /></td>
+              <td><asp:Button CssClass="operator-button" ID="Odown" runat="server" Text="&#8595;" OnClick="Odown_Click" Enabled="False" /></td>            
+
+            </tr>
+            <tr>
+                <td><asp:Button CssClass="operator-button" ID="Ndot" runat="server" Text="." OnClick="Ndot_Click" /></td> 
+                <td><asp:Button CssClass="number-button" ID="N0" runat="server" Text="0" OnClick="N0_Click" /></td> 
+                <td><asp:Button CssClass="operator-button" ID="Oremainder" runat="server" Text="%" OnClick="Oremainder_Click" /></td> 
+                <td><asp:Button CssClass="operator-button" ID="Odivide" runat="server" Text="/" OnClick="Odivide_Click1" /></td> 
+               <td><asp:Button CssClass="clear-button" ID="Oclear" runat="server" Text="C" OnClick="Oclear_Click" /></td> 
+            </tr>
+        </table>
+
+     </div>
+
+
+        
+        </asp:Panel>
+
+
+    </form>
+</body>
+</html>
---
+++ Calculator_Project/Calculator_083.aspx.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+
+//https://prod.liveshare.vsengsaas.visualstudio.com/join?4DA10F88C5BD92A10E8AB3AE33AE2F1584CD
+namespace Calculator_Project
+{
+    public static class GlobalData
+    {
+        public static string[] numbers1 = new string[100];
+        public static string[] numbers2 = new string[100];
+        public static string[] Operator = new string[100];
+        public static string[] AllAnswer = new string[100];
+    }
+
+    public partial class WebForm1 : System.Web.UI.Page
+    {        
+        Double FirstNo, SecondNo;
+        public static double arbitrary_ans = 0;
+        public static int counter =0, up_down_counter=0;
+        public static string arbitrary_operator;
+        void enter_number(double number)
+        {  
+            if (TextBox1.Text == "0" || TextBox1.Text.StartsWith("=")) {  TextBox1.Text = Convert.ToString(number);  }
+            else {  TextBox1.Text += Convert.ToString(number);  }
+        }
+
+        void perform_operation(string operators)
+        {
+            arbitrary_operator = operators;
+
+            if (TextBox1.Text.StartsWith("="))
+            { TextBox1.Text = "";}
+            else
+            { 
+            if (TextBox1.Text == ""){ errormessage.Text = "Please Enter any Number";}
+                else
+                {
+                    SecondNo = Convert.ToDouble(TextBox1.Text);
+                    FirstNo = arbitrary_ans;
+                    GlobalData.numbers1[counter] = Convert.ToString(arbitrary_ans);
+                    GlobalData.numbers2[counter] = Convert.ToString(TextBox1.Text);
+
+                    if (operators == "plus")
+                    {
+                        arbitrary_ans = FirstNo + SecondNo;
+                        GlobalData.Operator[counter] = "+"; }
+
+                    else if (operators == "minus")
+                    {
+                        arbitrary_ans = FirstNo - SecondNo;
+                        GlobalData.Operator[counter] = "-"; }
+
+                    else if (operators == "divide")
+                    {
+                        arbitrary_ans = FirstNo / SecondNo;
+                        GlobalData.Operator[counter] = "/"; }
+
+                    else if (operators == "multiply")
+                    {
+                        arbitrary_ans = FirstNo * SecondNo;
+                        GlobalData.Operator[counter] = "X"; }
+
+                    else
+                    {
+                        errormessage.Text = "Enter Valid Operation";
+                    }
+
+                    GlobalData.AllAnswer[counter] = Convert.ToString(arbitrary_ans);
+                    TextBox1.Text = "";
+                    up_down_counter = counter;
+                    counter++;
+                }
+            }
+        }
+        protected void Page_Load(object sender, EventArgs e) {  errormessage.Text = "";  }
+        protected void color_red_Click(object sender, EventArgs e)
+        { maindiv.BackColor = System.Drawing.Color.Red;  }
+        protected void N7_Click(object sender, EventArgs e){enter_number(7);}
+        protected void N8_Click(object sender, EventArgs e){enter_number(8);}
+        protected void N9_Click(object sender, EventArgs e){enter_number(9);}
+        protected void N4_Click(object sender, EventArgs e){enter_number(4);}
+        protected void N5_Click(object sender, EventArgs e){enter_number(5);}
+        protected void N6_Click(object sender, EventArgs e){ enter_number(6);}
+        protected void N1_Click(object sender, EventArgs e){ enter_number(1);}
+        protected void N2_Click(object sender, EventArgs e){enter_number(2);}
+        protected void N3_Click(object sender, EventArgs e){enter_number(3);}
+        protected void N0_Click(object sender, EventArgs e){enter_number(0);}
+        protected void Oplus_Click(object sender, EventArgs e){ perform_operation("plus"); }
+        protected void Ominus_Click(object sender, EventArgs e){ perform_operation("minus"); }
+        protected void Omultiply_Click(object sender, EventArgs e){ perform_operation("multiply"); }
+        protected void Odivide_Click(object sender, EventArgs e) { perform_operation("divide"); }
+        protected void Oclear_Click(object sender, EventArgs e)
+        {
+            counter = 0;
+            up_down_counter = 0;
+            arbitrary_operator = null;
+            arbitrary_ans = 0;
+            TextBox1.Text = "0";
+            Odown.Enabled = false;
+            Oup.Enabled = false;
+
+        }
+
+        protected void green_Click(object sender, EventArgs e)
+        {
+            maindiv.BackColor = System.Drawing.Color.Green;
+        }
+        protected void blue_Click(object sender, EventArgs e)
+        {
+            
+            maindiv.BackColor = System.Drawing.Color.Blue;
+        }
+        protected void red_Click(object sender, EventArgs e)
+        {maindiv.BackColor = System.Drawing.Color.Red; }
+        protected void Calculate_Click(object sender, EventArgs e)
+        {
+            perform_operation(arbitrary_operator);
+            TextBox1.Text = Convert.ToString("= " +arbitrary_ans);
+            Odown.Enabled = true;
+            Oup.Enabled = true;
+        }
+
+        protected void black_Click(object sender, EventArgs e)
+        { maindiv.BackColor = System.Drawing.Color.Black; }
+
+        protected void Oback_Click(object sender, EventArgs e)
+        {
+            if (TextBox1.Text == "" || TextBox1.Text.StartsWith("=")){TextBox1.Text = "";}
+            else
+            {TextBox1.Text = TextBox1.Text.Remove(TextBox1.Text.Length - 1);}
+        }
+        protected void Odivide_Click1(object sender, EventArgs e){ perform_operation("divide"); }
+        protected void Oremainder_Click(object sender, EventArgs e)
+        {
+            arbitrary_operator = "remainder";
+
+            if (TextBox1.Text.StartsWith("="))
+            {
+                TextBox1.Text = "";
+            }
+            else
+            {
+                if (TextBox1.Text == "")
+                {
+                    errormessage.Text = "Please Enter any Number";
+                }
+                else
+                {
+                    SecondNo = Convert.ToDouble(TextBox1.Text);
+                    FirstNo = arbitrary_ans;
+
+
+                    arbitrary_ans = FirstNo % SecondNo;
+
+                    TextBox1.Text = "";
+                }
+            }
+
+        }
+        protected void Oup_Click(object sender, EventArgs e)
+        {
+            if (up_down_counter >= 1)
+            {
+                TextBox1.Text = GlobalData.numbers1[up_down_counter] + " " + GlobalData.Operator[up_down_counter] + " " + GlobalData.numbers2[up_down_counter] + " = " + GlobalData.AllAnswer[up_down_counter];
+                up_down_counter--;
+            }
+
+            else
+            {
+                errormessage.Text = "No Previous Calculation";
+            }
+
+        }
+
+        protected void Odown_Click(object sender, EventArgs e)
+        {
+            if (up_down_counter<counter-1)
+            {
+                up_down_counter++;
+                TextBox1.Text = GlobalData.numbers1[up_down_counter] + " " + GlobalData.Operator[up_down_counter] + " " + GlobalData.numbers2[up_down_counter] + " = " + GlobalData.AllAnswer[up_down_counter];
+               
+            }
+
+            else
+            {
+                errormessage.Text = "It's Your Latest Calculation";
+
+            }
+        }
+
+        protected void Ndot_Click(object sender, EventArgs e)
+        {
+            if (TextBox1.Text.EndsWith("."))
+            {
+                errormessage.Text = "Cann't add double decimal";
+            }
+            else
+            {
+                if (TextBox1.Text == "0" || TextBox1.Text.StartsWith("="))
+                {
+                    TextBox1.Text = "0.";
+
+                }
+
+                else
+                {
+                    TextBox1.Text += ".";
+                }
+            }
+        }
+
+    }
+}
